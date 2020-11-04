ea15881c2bd80b7659d976e9aa87901aa58421c5
anyou <anyou.6@gmail.com>
2020-11-04 23:34:22 +0100

    premier commit to do list

---
+++ main.js
@@ -0,0 +1,22 @@
+//let champ = document.getElementById("champ");
+
+form.addEventListener("submit", function(event){
+    const li = document.createElement("li");
+    li.textContent = champ.value;
+    listeDeTaches.appendChild(li)
+    champ.value = " ";
+    event.preventDefault();
+  })
+  
+  /*
+  boutonAjouter.addEventListener("click", function(){
+    const li = document.createElement("li");
+    li.textContent = champ.value;
+    listeDeTaches.appendChild(li)
+    champ.value = " ";
+  })
+  */
+  
+  //boutonAjouter.onclick = () => alert("hello"); function fléchée sans paramètre
+  //boutonAjouter.onclick = (prenom) => hello("Anyou"); function fléchée avec paramètre
+  console.log(champ.value)
