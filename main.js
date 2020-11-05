117ad3a71322a091c823ddfd3c10477d68987a14
anyou <anyou.6@gmail.com>
2020-11-05 21:49:40 +0100

    tuto to do list vidéo numéro 7 déjà regardé

--- main.js
+++ main.js
@@ -1,10 +1,24 @@
+"use strict";
+
+  // ajouter une tache avec la methode addEventListener()
+
 form.addEventListener("submit", function(event){
-    const li = document.createElement("li");
-    li.textContent = champ.value;
+    let li = document.createElement("li");
+    let spanElt = document.createElement("span");
+    li.innerHTML = champ.value;
+    spanElt.textContent = " x";
+    spanElt.onclick = () => removeTache(li);
     listeDeTaches.appendChild(li)
+    li.appendChild(spanElt)
     champ.value = " ";
     event.preventDefault();
   })
+
+  // function effacer une tache avec la methode remove()
+
+function removeTache(tache){
+    tache.remove();
+}
   
   /*
   boutonAjouter.addEventListener("click", function(){
