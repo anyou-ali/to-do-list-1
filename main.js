40793ebd20b2b1f94e45b093e63ce1ce491952fb
anyou <anyou.6@gmail.com>
2020-11-10 00:00:00 +0100

    tuto to do list video numéro 14 déjà regardé

--- main.js
+++ main.js
@@ -17,24 +17,34 @@ for(let element of removeSpanElt){
 
 // ajouter une tache avec la methode addEventListener()
 
-form.addEventListener("submit", function(event){
+form.addEventListener("submit", function(){
     let liElt = document.createElement("li");
     let spanElt = document.createElement("span");
     spanElt.classList.add("removeliElt", "material-icons");
-    liElt.innerHTML = champTache.value;
     spanElt.textContent = "delete_forever";
+    
+    let spanEltImportant = document.createElement("span");
+    spanEltImportant.classList.add("eltImportant", "material-icons");
+    spanEltImportant.textContent = "stars";
+    
+    liElt.innerHTML = champTache.value;
+    
     spanElt.onclick = () => removeTache(liElt);
+    spanEltImportant.onclick = () => tacheImportant(spanEltImportant);
     //spanElt.addEventListener("click",function(){removeTache(liElt)})
     listeDeTaches.appendChild(liElt)
+    liElt.appendChild(spanEltImportant)
     liElt.appendChild(spanElt)
     champTache.value = " ";
     pasDeTache.style.display = "none";
 
     // sauvegarde des tâches dans le localStorage
     
-    localStorage.setItem("listTache", listeDeTaches.innerHTML)    
+    localStorage.setItem("listTache", listeDeTaches.innerHTML)
+    
+    return false 
 
-    event.preventDefault();
+    //event.preventDefault(); // rechercher et bien comprendre ce que fait event.preventDefault();
   })
 
 // function effacer une tache avec la methode remove()
@@ -48,6 +58,12 @@ function removeTache(tache){
     // enregistrement de la nouvelle liste lors de l'actualisation de la page
     
     localStorage.setItem("listTache", listeDeTaches.innerHTML)
+}
+
+// function toggle pour marquer une tache comme importante
+
+function tacheImportant(element){
+    element.classList.toggle("importantLi");
 }
   
   /*
