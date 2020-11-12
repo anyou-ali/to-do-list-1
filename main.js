4278abbcaba0e042090fd3fac2a7e70342051935
anyou <anyou.6@gmail.com>
2020-11-12 23:26:40 +0100

    tuto todo list version 1 terminé

--- main.js
+++ main.js
@@ -106,15 +106,6 @@ window.onbeforeinstallprompt = (event) => {
 //     return false
 // }
   
-  /*
-  boutonAjouter.addEventListener("click", function(){
-    const li = document.createElement("li");
-    li.textContent = champ.value;
-    listeDeTaches.appendChild(li)
-    champ.value = " ";
-  })
-  */
-  
-  //boutonAjouter.onclick = () => alert("hello"); function fléchée sans paramètre
-  //boutonAjouter.onclick = (prenom) => hello("Anyou"); function fléchée avec paramètre
-  //console.log(champTache.value)
+//boutonAjouter.onclick = () => alert("hello"); function fléchée sans paramètre
+//boutonAjouter.onclick = (prenom) => hello("Anyou"); function fléchée avec paramètre
+//console.log(champTache.value)
